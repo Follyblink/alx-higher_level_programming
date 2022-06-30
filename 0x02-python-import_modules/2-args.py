f4aa6314f26c1c46f2c1cc77e7a580217776956d
follyblink <follyblink@gamil.com>
2022-06-30 15:15:47 +0000

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+        from sys import argv
+        total = len(argv)
+        if total <= 1:
+            print("0 arguments.")
+        else:
+            if total == 2:
+                print(f'{(total - 1):d} argument:')
+            else:
+                print(f'{(total - 1):d} arguments:')
+            for i in range(1, total):
+                print(f'{i:d}: {argv[i]:s}')
